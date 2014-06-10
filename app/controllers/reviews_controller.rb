3ed8654ffb8a10748c0948e05cf5b30e92a9f288
vagrant <vagrant@kellogg-dev-box>
2014-06-10 14:36:23 +0000

    conditional views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,15 @@
 class ReviewsController < ApplicationController
 
   def create
+  user = User.find_by("id" => cookies["user_id"])
+  if user.present?
     Review.create("place_id" => params["place_id"],
                   "rating" => params["rating"],
-                  "comments" => params["content"])
-    redirect_to "/places/#{params["places_id"]}", :notice => "Thanks for your review!"
+                  "comments" => params["content"], "user_id" => cookies["user_id"])
+    redirect_to "/places/#{params["place_id"]}", :notice => "Thanks for your review!"
+  else
+  	redirect_to "/places/#{params["place_id"]}", :notice => "Please sign in to write a review"
+  end
 
   end
 
